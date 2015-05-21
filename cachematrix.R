d172c40321582ced7782b11f58578af6fc36f5ec
markpmilton <mark.milton@britinsurance.com>
2015-05-21 15:10:44 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        
+        inv = x$getinv()
+        
+        ## is there a cached version
+        if (!is.null(inv)){
+                # go get the cache version
+                message("go get a cup of tea I going for cached data")
+                return(inv)
+        }
+        
+        ## calculates the inverse 
+        mat.data = x$get()
+        inv = solve(mat.data, ...)
+        
+        ## used the invs function to set the inverse
+        x$setinv(inv)
+        
+        return(inv)
+}
+makeCacheMatrix <- function(x = matrix()) {
+        ## This should take a matrix and produce the inverse of it
+        ## Following the following stages
+        ## Stage 1) setup a matrix
+        ## Stage 2) get the input matrix and put it into the newly setup matrix
+        ## Stage 3) compute the inverse
+        ## Stage 4) put the inverse into the matrix
+        
+        
+        ##Stage 1)
+        matInverse = NULL
+        set = function(y) {
+                ## "<<-" Gives object value in a different environment
+                x<<-y
+                matInverse <<-NULL
+        }
+        
+        ##Stage2)
+        get = function() x
+        
+        ##Stage3)
+        set = function(inverse) matInverse
+        
+        ##Stage4) 
+        setmatinverse = function(inverse) matInverse <<- inverse
+        getmatinverse = function() matInverse
+        list(set=set, get=get, setmatinverse=setmatinverse, getmatinverse=getmatinverse)
+}
+test = function(mat){
+        ## @mat: an invertible matrix
+        
+        temp = makeCacheMatrix(mat)
+        
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
 }
